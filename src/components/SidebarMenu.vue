53c685b08a613dc0b9797f87379bf26e4208364b
Mesya Maureen Jeynie <mesya.jeynie@student.htw-berlin.de>
2024-06-13 18:25:21 +0000

    85, 86, 63 startseite und navigationsmöglichkeiten

---
+++ src/components/SidebarMenu.vue
@@ -0,0 +1,83 @@
+<template>
+  <sidebar-menu :menu="menu" :hide-toggle="true" :width="'80px'">
+    <template v-slot:toggle-icon>DevEco</template>
+  </sidebar-menu>
+</template>
+
+<script lang="ts">
+import { h } from 'vue'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+const separator = h('hr', {
+  style: {
+    borderColor: 'rgba(0, 0, 0, 0.1)',
+    margin: '20px'
+  }
+})
+
+const faIcon = (props: any) => {
+  return {
+    element: h('div', [h(FontAwesomeIcon, { size: 'lg', ...props })])
+  }
+}
+
+export default {
+  data() {
+    return {
+      menu: [
+        {
+          header: 'DevEco',
+          hiddenOnCollapse: true
+        },
+        {
+          href: '/',
+          icon: faIcon({ icon: 'fa-solid fa-chart-simple' })
+        },
+        {
+          href: '/ressourcen',
+          icon: faIcon({ icon: 'fa-solid fa-database' })
+        },
+        {
+          href: '/pipelines',
+          icon: faIcon({ icon: 'fa-solid fa-code-fork' })
+        },
+        {
+          href: '/code',
+          icon: faIcon({ icon: 'fa-solid fa-code' })
+        },
+        {
+          href: '/organisatorisch',
+          icon: faIcon({ icon: 'fa-solid fa-building' })
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+.v-sidebar-menu {
+  --vsm-primary-color: #4285f4;
+  --vsm-base-bg: #ebebeb;
+  --vsm-item-color: #fff;
+  --vsm-item-active-line-color: var(--vsm-primary-color);
+  --vsm-item-open-color: #fff;
+  --vsm-item-open-bg: var(--vsm-primary-color);
+  --vsm-item-hover-bg: #dfedd6;
+  --vsm-icon-color: var(--vsm-item-color);
+  --vsm-icon-bg: #fff;
+  --vsm-mobile-item-color: #fff;
+  --vsm-mobile-item-bg: var(--vsm-primary-color);
+  --vsm-mobile-icon-color: var(--vsm-mobile-item-color);
+  --vsm-mobile-icon-bg: transparent;
+  --vsm-dropdown-bg: #36363b;
+  --vsm-header-item-color: rgba(255, 255, 255, 0.7);
+  --vsm-toggle-btn-color: #fff;
+  --vsm-toggle-btn-bg: #1e1e21;
+  --vsm-item-font-size: 16px;
+  --vsm-item-line-height: 35px;
+  --vsm-item-padding: 10px 15px;
+  --vsm-icon-height: 35px;
+  --vsm-icon-width: 35px;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,15 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueSidebarMenu from 'vue-sidebar-menu'
+import 'vue-sidebar-menu/dist/vue-sidebar-menu.css'
+import './fontawesome'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 const app = createApp(App)
 
-app.use(router)
+app.use(VueSidebarMenu)
+app.component('font-awesome-icon', FontAwesomeIcon)
 
+app.use(router)
 app.mount('#app')
