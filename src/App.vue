db2f53031581102bb14d815b4e76f1d3a60c441a
s0568984 <s0568984@Student.HTW-Berlin.de>
2024-06-24 11:30:10 +0200

    Grundgerüst für Kacheln erstellen

--- src/App.vue
+++ src/App.vue
@@ -4,86 +4,101 @@ import sidebarNav from '@/components/SidebarMenu.vue'
 import type dashboard from './components/cDashboard.vue'
 </script>
 
+
 <template>
-  <sidebar-nav />
-  <RouterView />
+ <sidebar-nav />
+ <RouterView />
 </template>
+
 
 <style scoped>
 header {
-  line-height: 1.5;
-  max-height: 100vh;
-  margin: 2rem 2rem 0 0;
-  width: 100%;
-  display: flex;
-  flex-direction: row;
-  padding-left: 175px;
+ line-height: 1.5;
+ max-height: 100vh;
+ margin: 2rem 2rem 0 0;
+ width: 100%;
+ display: flex;
+ flex-direction: row;
+ padding-left: 175px;
 }
+
+
 .content {
-  padding-left: 175px;
+ padding-left: 175px;
 }
+
 
 .logo {
-  display: flex;
-  place-items: center;
-  margin: 0 0 0 0;
+ display: flex;
+ place-items: center;
+ margin: 0 0 0 0;
 }
+
 
 nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: end;
-  margin-top: 2rem;
+ width: 100%;
+ font-size: 12px;
+ text-align: end;
+ margin-top: 2rem;
 }
+
 
 nav a.router-link-exact-active {
-  color: var(--color-text);
+ color: var(--color-text);
 }
+
 
 nav a.router-link-exact-active:hover {
-  background-color: #ffffff;
+ background-color: #ffffff;
 }
+
 
 nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
+ display: inline-block;
+ padding: 0 1rem;
+ border-left: 1px solid var(--color-border);
 }
+
 
 nav a:first-of-type {
-  border: 0;
+ border: 0;
 }
+
 
 @media (min-width: 1024px) {
-  header {
-    display: flex;
-    flex-direction: row;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-    width: fit-content;
-  }
+ header {
+   display: flex;
+   flex-direction: row;
+   place-items: center;
+   padding-right: calc(var(--section-gap) / 2);
+   width: fit-content;
+ }
 
-  .logo {
-    margin: 2rem 1rem 0 0;
-    display: flex;
-    place-items: center;
-  }
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-    flex-direction: column;
-  }
+ .logo {
+   margin: 2rem 1rem 0 0;
+   display: flex;
+   place-items: center;
+ }
 
-  nav {
-    text-align: end;
-    margin-left: -1rem;
-    font-size: 1rem;
-    display: flex;
-    justify-content: end;
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+
+ header .wrapper {
+   display: flex;
+   place-items: flex-start;
+   flex-wrap: wrap;
+   flex-direction: column;
+ }
+
+
+ nav {
+   text-align: end;
+   margin-left: -1rem;
+   font-size: 1rem;
+   display: flex;
+   justify-content: end;
+   padding: 1rem 0;
+   margin-top: 1rem;
+ }
 }
 </style>
+
