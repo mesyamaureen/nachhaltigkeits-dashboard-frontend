1812473009a17385a193ae41f3620dfc2bc3df36
mesyamaureen <mesyamaureen@gmail.com>
2024-07-10 21:35:28 +0200

    chart bibliothek installiert

---
+++ src/components/phase/chart.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <line-chart :data="datacollection" :options="options"></line-chart>
+  </div>
+</template>
+
+<script lang="ts">
+import { Line } from 'vue-chartjs'
+
+export default {
+  components: {
+    LineChart: Line
+  },
+  data() {
+    return {
+      datacollection: {
+        labels: ['April', 'May', 'June'],
+        datasets: [
+          {
+            label: 'Energieverbrauch',
+            backgroundColor: '#f87979',
+            data: [40, 60, 50]
+          },
+          {
+            label: 'Gesamt Transport weg',
+            backgroundColor: '#7CB342',
+            data: [50, 70, 60]
+          }
+        ]
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+/* Add your styles here */
+</style>
