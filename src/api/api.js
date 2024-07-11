c846274f60f82d5b38c19e09c4748833aca9ff2e
mesyamaureen <mesyamaureen@gmail.com>
2024-07-11 23:59:12 +0200

    endpoint nach dem neuen endpoint im Backend angepasst

--- src/api/api.js
+++ src/api/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const fetchKpi = async () => {
     try {
       const baseUrl = import.meta.env.VITE_APP_BACKEND_BASE_URL;
-      const endPoint = `${baseUrl}/api/kpis`
+      const endPoint = `${baseUrl}/kpis`
       const response = await axios.get(endPoint); // Passe die URL entsprechend an
       // Füge die `visible` Eigenschaft hinzu
       const kpis = response.data.map(kpi => ({ ...kpi, visible: false }));
